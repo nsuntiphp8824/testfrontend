d74427fbb10f626f7534e14bbe84222ba7299521
nsuntiphp@gmail.com <nsuntiphp@gmail.com>
2020-12-30 20:22:20 +0700

    config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_15nDMvr2K', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7qjniknkfqefdlmrmvr0m7cu0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_kqc0EaXDd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3rs9e96l23o365vs8ir5sqj6ng', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://miq87fm8ef.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
