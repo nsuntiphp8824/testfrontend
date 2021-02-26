ff35bfd9bcd4496886e8c2735cfa8fa4cddda440
nsuntiphp@gmail.com <nsuntiphp@gmail.com>
2021-02-26 21:48:49 +0700

    update

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
-    cognito: {
-        userPoolId: 'ap-southeast-1_kqc0EaXDd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3rs9e96l23o365vs8ir5sqj6ng', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: 'https://s4kp1spzv4.execute-api.ap-southeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-1_lbfGUHTxm", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "5qs22nsiq2tn0h404rk9gclqe2", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl:
+      "https://s4kp1spzv4.execute-api.ap-southeast-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
